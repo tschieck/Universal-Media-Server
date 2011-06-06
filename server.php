5359bbc7c9eccbb649e9c5f2a48f3f8fa1ebba9c
Tim Schieck <tim@jacensolo.com>
2011-06-06 10:23:27 -0600

    test commit

---
+++ server.php
@@ -0,0 +1,38 @@
+<?php
+require("include/config.php");
+header("Content-Type: text/xml");
+db_con();
+mysql_select_db("media");
+ 
+$command = $_GET["mode"];
+
+?>
+<?php //<xml> ?>
+    <?php
+    if ($command == "TV") {
+        $query = mysql_query("SELECT * FROM TV_Shows");
+        echo "<shows>\n";
+        
+        while ($shows = mysql_fetch_array($query)) {
+            echo "<show>\n";
+            echo "\t<id>" . $shows["ID"] . "</id>\n";
+            echo "\t<name>" . $shows["Name"] . "</name>\n";
+            echo "\t<seasons>" . $shows["Seasons"] . "</seasons>\n";
+            echo "\t<episodes>" . $shows["Episodes"] . "</episodes>\n";
+            echo "</show>\n";
+        }
+        echo "</shows>\n";
+
+        if (isset($_GET["show"])) {
+           
+            $query = mysql_query("SELECT * FROM TV_Episodes WHERE ");
+        }
+    }   else {
+        echo "<version>" . $software["Version"] . "</version>";
+        echo "<modules>" . $software["Modules"] . "</modules>";
+
+    }
+    ?>
+<?php //</xml> ?>
+
+
